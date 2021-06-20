8c0a0077d4b823249ed7eeb0269552d2db206aa1
hultdin <34416082+hultdin@users.noreply.github.com>
2021-06-20 15:59:59 +0200

    Update promiscuous.sh

--- promiscuous.sh
+++ promiscuous.sh
@@ -22,20 +22,19 @@ fi
 # set the physical interface to promiscuous mode
 if [ `xe pif-param-get uuid=${uuid} param-name=other-config param-key=promiscuous` != "true" ]; then
   echo "xe pif-param-set uuid=${uuid} other-config:promiscuous=\"true\""
-  xe pif-param-set uuid=${uuid} other-config:promiscuous="true"
+  xe pif-param-set uuid=${uuid} other-config:promiscuous=\"true\"
 fi
 
 # set all virtual interfaces associated with the physical interface to promiscuous mode
 for uuid in `xe vif-list network-name-label=${pif} params=uuid | sed '/^\s*$/d' | awk '{print $5}'`; do
   if [ `xe vif-param-get uuid=${uuid} param-name=other-config param-key=promiscuous` != "true" ]; then
     echo "xe vif-param-set uuid=${uuid} other-config:promiscuous=\"true\""
-    xe vif-param-set uuid=${uuid} other-config:promiscuous="true"
+    xe vif-param-set uuid=${uuid} other-config:promiscuous=\"true\"
   fi
 done
 
 # connect all virtual interfaces associated with the physical interface to the existing bridge
 for vif in `ovs-vsctl list-ifaces ${br} | grep -v ${pif}`; do
-  echo "ovs-vsctl -- set Bridge ${br} mirrors=@m -- --id=@${pif} get Port ${pif} -- --id=@${pif} get Port ${pif} -- --id=@m create Mirror name=${pif}-mirror select-src-port=@${pif} select-dst-port=@${pif} output-port=@${vif}
-"
-  ovs-vsctl -- set Bridge ${br} mirrors=@m -- --id=@${pif} get Port ${pif} -- --id=@${pif} get Port ${pif} -- --id=@m create Mirror name=${pif}-mirror select-src-port=@${pif} select-dst-port=@${pif} output-port=@${vif}
+  echo "ovs-vsctl -- set Bridge ${br} mirrors=@m -- --id=@${pif} get Port ${pif} -- --id=@${vif} get Port ${vif} -- --id=@m create Mirror name=${pif}-mirror select-src-port=@${pif} select-dst-port=@${pif} output-port=@${vif}"
+  ovs-vsctl -- set Bridge ${br} mirrors=@m -- --id=@${pif} get Port ${pif} -- --id=@${vif} get Port ${vif} -- --id=@m create Mirror name=${pif}-mirror select-src-port=@${pif} select-dst-port=@${pif} output-port=@${vif}
 done
